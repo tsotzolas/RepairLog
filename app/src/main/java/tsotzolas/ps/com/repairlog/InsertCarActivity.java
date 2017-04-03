82f8362166cf70267668b2176343854da24ecc02
tsotzolas@gmail.com <xan5412@git>
2017-04-03 15:09:14 +0300

    Continue to develop Repair Log

--- app/src/main/java/tsotzolas/ps/com/repairlog/InsertCarActivity.java
+++ app/src/main/java/tsotzolas/ps/com/repairlog/InsertCarActivity.java
@@ -1,24 +1,17 @@
 package tsotzolas.ps.com.repairlog;
 
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.util.Locale;
 
 /**
  * Created by tsotzolas on 3/4/2017.
  */
 
-public class ChooseToInsertActivity extends AppCompatActivity{
+public class InsertCarActivity extends AppCompatActivity{
 
     private Button changeLanguageButton;
 
@@ -27,7 +20,7 @@ public class ChooseToInsertActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_to_insert_main);
+        setContentView(R.layout.insert_car);
 
 
     }
